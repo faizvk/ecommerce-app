1a0d80a4f34db5d487c00b2dfdf091c2150d3c74
faizvk <faizvk14@gmail.com>
2026-02-01 17:25:21 +0530

    update server loading screen

--- ecommerce-frontend/src/components/ServerLoadingScreen.jsx
+++ ecommerce-frontend/src/components/ServerLoadingScreen.jsx
@@ -6,7 +6,11 @@ const ServerLoadingScreen = () => {
       <h2>Starting backend…</h2>
       <p>Render free tier sleeps after inactivity. Please wait.</p>
       <p>Meanwhile ui screenshots are available in my github repo</p>
-      <p>https://github.com/faizvk/ecommerce-app</p>
+      <p>
+        <a href="https://github.com/faizvk/ecommerce-app" target="_blank">
+          https://github.com/faizvk/ecommerce-app
+        </a>
+      </p>
     </div>
   );
 };
